1a03a602d37027ba9919dd9e77dbe18c7d1fb490
ML and Web Deploying <codewithAkshaya68@gmail.com>
2026-04-29 16:06:10 +0530

    Create AirlineTicketBookingApplication.java

---
+++ src/main/java/com/akshaya/booking/AirlineTicketBookingApplication.java
@@ -0,0 +1,11 @@
+package com.akshaya.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketBookingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineTicketBookingApplication.class, args);
+    }
+}
